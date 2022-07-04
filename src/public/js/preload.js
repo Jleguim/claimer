3d0cb5ff06f69644ecc24c4842299668f90edc08
Javier Leguizamo <jlegui745@gmail.com>
2022-07-03 22:13:47 -0500

    Maintain JWT in app using ipcMain.handle

--- src/public/js/preload.js
+++ src/public/js/preload.js
@@ -4,3 +4,7 @@ contextBridge.exposeInMainWorld('requests', {
     login: (username, password) => ipcRenderer.invoke('login', username, password),
     getProducts: (jwt) => ipcRenderer.invoke('getProducts', jwt)
 })
+
+contextBridge.exposeInMainWorld('keys', {
+    jwt: (token) => ipcRenderer.invoke('jwt', token)
+})
