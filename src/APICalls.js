89f4b19cd7c2f69280b769f28256a56fb82843bf
Javier Leguizamo <jlegui745@gmail.com>
2022-07-03 22:11:05 -0500

    Move handles.js to APICalls.js

--- src/APICalls.js
+++ src/APICalls.js
@@ -12,9 +12,6 @@ ipcMain.handle('getProducts', (event, jwt) => {
             .then((res) => {
                 var set_cookie = res.header['set-cookie']
 
-                console.log(res.header)
-
-
                 var jwt_cookie = set_cookie[0]
                 var newJwt = jwt_cookie.split('; ')[0].split('=')[1]
 
@@ -38,16 +35,6 @@ ipcMain.handle('login', (event, username, password) => {
                 var newJwt = jwt_cookie.split('; ')[0].split('=')[1]
 
                 resolve({ jwt: newJwt, text: res.text, body: res.body })
-
-                // var cookies = {}
-                // set_cookie.map(cookie => {
-                //     var params = cookie.split('; ')
-                //     params.map(param => {
-                //         var data = param.split('=')
-                //         cookies[data[0]] = data[1]
-                //     })
-                // })
-                // console.log(cookies)
             })
             .catch(reject)
     })
