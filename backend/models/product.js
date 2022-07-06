d4c529609501207dce498b05c235bced740d1f78
Javier Leguizamo <jlegui745@gmail.com>
2022-07-06 02:22:26 -0500

    Updated product schema

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,8 +1,11 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const fs = require('fs')
 
 var productSchema = new mongoose.Schema({
-    type: String,
-    key: String
+    name: { type: String, required: true },
+    price: { type: Number, required: true, default: 10 },
+    pictureUrl: { type: String, required: true, default: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' },
+    picture: { type: Buffer, required: true, default: fs.readFileSync(__dirname + '\\..\\..\\src\\public\\logo.png') }
 })
 
 module.exports = mongoose.model('Product', productSchema)
