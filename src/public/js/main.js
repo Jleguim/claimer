cea3c4bc6a6f80f27ccefb33413ad5bf1f0a7f5e
Javier Leguizamo <jlegui745@gmail.com>
2022-07-06 02:25:43 -0500

    Use an API call for products

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -2,24 +2,29 @@ var contentDiv = document.getElementById('content')
 
 // stuff that should be in a db call 
 var points = 1337
-var products = [
-    { name: 'Cool Rank', price: 123, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' },
-    { name: 'T-shirt', price: 321, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' },
-    { name: 'Red Jacket', price: 53, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' },
-    { name: 'Product', price: 12, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' },
-    { name: 'Dust', price: 1, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' },
-    { name: 'More Dust', price: 1, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' }
-]
+// var products = [
+//     { name: 'Cool Rank', price: 123, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' },
+//     { name: 'T-shirt', price: 321, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' },
+//     { name: 'Red Jacket', price: 53, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' },
+//     { name: 'Product', price: 12, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' },
+//     { name: 'Dust', price: 1, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' },
+//     { name: 'More Dust', price: 1, picture: 'https://cdn.discordapp.com/attachments/451359497546498051/993374995638140958/logo.png' }
+// ]
 // ------
 
 
 function renderPage() {
     console.log(currentPage)
     if (currentPage == 0) renderProductClaimer()
+    if (currentPage == 1) contentDiv.innerHTML = ''
+    if (currentPage == 2) contentDiv.innerHTML = ''
 }
 
-function renderProductClaimer() {
+async function renderProductClaimer() {
     contentDiv.innerHTML = ''
+
+    var getResponse = await window.requests.getProducts()
+    var products = getResponse.body
 
     var grid = document.createElement('div')
     grid.classList.add('grid', 'grid-cols-1', 'grid-rows-1', 'gap-4', 'lg:grid-cols-5', 'sm:grid-cols-3', 'md:grid-cols-4')
@@ -29,7 +34,7 @@ function renderProductClaimer() {
         var productElement = document.createElement('claimer-product')
         productElement.setAttribute('name', product.name)
         productElement.setAttribute('price', product.price)
-        productElement.setAttribute('img', product.picture)
+        productElement.setAttribute('img', product.pictureUrl)
         grid.appendChild(productElement)
 
         console.log(product)
