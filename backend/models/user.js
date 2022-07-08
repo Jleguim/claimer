97bb0940f4591b36458096964359680a54a952aa
Javier Leguizamo <jlegui745@gmail.com>
2022-07-07 22:13:04 -0500

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 
 var userSchema = new mongoose.Schema({
+    discordId: { type: String, required: true, unique: true },
     username: { type: String, required: true },
     password: { type: String, required: true }
 })
