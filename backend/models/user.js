852af85809de5f64a2144785168d8742d5cca394
Javier Leguizamo <jlegui745@gmail.com>
2022-07-07 19:57:05 -0500

    Implement bcrypt

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,20 @@
-var mongoose = require('mongoose')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 var userSchema = new mongoose.Schema({
-    username: String,
-    password: String
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+})
+
+userSchema.method('hashPassword', function (password) {
+    var hashed = bcrypt.hashSync(password, 10)
+    this.password = hashed
+    return this
+})
+
+userSchema.method('checkPassword', function (plainPass) {
+    var hashedPass = this.password
+    return bcrypt.compareSync(plainPass, hashedPass)
 })
 
 module.exports = mongoose.model('User', userSchema)
