ec9cd747acecd45c27681e91742eb3442b9f6b85
Javier Leguizamo <jlegui745@gmail.com>
2022-07-07 19:57:27 -0500

    Cleanup and use new auth controllers

--- backend/routes.js
+++ backend/routes.js
@@ -1,16 +1,24 @@
 const { json } = require('body-parser')
 
-const jwt = require('./jwt/index.js')
+const jwt = require('./auth/').jwt
+
 const controllers = require('./controllers.js')
+const auth = require('./auth/controllers')
+
+const DISCORD_CALLBACK_ROUTE = process.env.DISCORD_CALLBACK_ROUTE
 
 module.exports = (app) => {
     app.use('/auth/', json())
 
-    app.get('/auth/login', controllers.login)
-    app.get('/auth/extend', controllers.extendToken)
-    app.get('/auth/check', jwt.isAuthorized, (req, res) => {
-        res.status(200).send({ message: 'Authed!' })
-    })
+    // jwt
+    app.get('/auth/login', auth.login)
+    app.get('/auth/extend', auth.extendJWT)
+    app.get('/auth/check', jwt.isAuthorized, auth.checkJWT)
 
+    // discord
+    app.get('/auth/discord', auth.redirectDiscordAuthUrl)
+    app.get(DISCORD_CALLBACK_ROUTE, auth.discordAuthCallback)
+
+    // secured routes
     app.get('/api/products', jwt.isAuthorized, controllers.getProducts)
 }
