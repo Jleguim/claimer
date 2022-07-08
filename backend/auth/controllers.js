7218840b63b19de89164a897b1c47b285929fbd9
Javier Leguizamo <jlegui745@gmail.com>
2022-07-07 19:56:45 -0500

    Move auth-related controllers to `auth` folder

---
+++ backend/auth/controllers.js
@@ -0,0 +1,86 @@
+const jwt = require('./jwt/')
+const discord = require('./discord')
+
+const DEFAULT_USER_PASSWORD = process.env.DEFAULT_USER_PASSWORD
+
+// JWT stuff
+module.exports.login = async (req, res) => {
+    if (req.body.username | req.body.password == undefined) {
+        return res.status(400).send({ message: 'Bad Request' })
+    }
+
+    try {
+        var User = mongoose.models.User
+        var queriedUser = await User.findOne({ username })
+
+        if (!queriedUser) {
+            res.status(403).send({ message: 'Forbidden', code: 1 })
+            return
+        }
+
+        if (queriedUser.checkPassword(password) == false) {
+            res.status(403).send({ message: 'Forbidden', code: 2 })
+            return
+        }
+
+        var token = await jwt.sign({ username })
+        res.cookie('jwt', token)
+        res.status(200).send({ message: 'Authenticated!' })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({ message: 'Internal server error' })
+    }
+}
+
+module.exports.extendJWT = async (req, res) => {
+    if (!req.cookies.jwt) {
+        res.status(403).send({ message: 'Forbidden' })
+        return
+    }
+
+    try {
+        var data = await jwt.verify(req.cookies.jwt)
+        var newToken = await jwt.sign({ username: data.username })
+
+        res.cookie('jwt', newToken)
+        res.status(200).send({ message: 'Extended!' })
+    } catch (error) {
+        console.log(error.message)
+
+        if (error.message != 'jwt expired') {
+            return res.status(500).send({ message: 'Internal server error' })
+        }
+
+        res.status(403).send({ message: 'Forbidden' })
+    }
+}
+
+module.exports.checkJWT = (req, res) => res.status(200).send({ message: 'Authed!' })
+// --------
+
+// Discord stuff
+module.exports.discordAuthCallback = async (req, res) => {
+    if (!req.query.code) {
+        res.status(400).send({ message: 'Bad Request' })
+        return
+    }
+
+    try {
+        var body = await discord.requestToken(req.query.code)
+        var userData = await discord.getUser(body.access_token)
+
+        var User = mongoose.models.User
+        var user = new User({ username: userData.username })
+
+        user.hashPassword(DEFAULT_USER_PASSWORD)
+        await user.save()
+
+        res.status(200).send({ message: 'Created account with Discord' })
+    } catch (error) {
+        if (!error.response) return console.log(error.message)
+        console.log(error.response.body)
+    }
+}
+
+module.exports.redirectDiscordAuthUrl = (req, res) => res.redirect(discord.authUrl)
+// -------------
--- backend/controllers.js
+++ backend/controllers.js
@@ -1,48 +1,4 @@
 const mongoose = require('mongoose')
-const jwt = require('./jwt/')
-
-module.exports.login = async (req, res) => {
-    var username = req.body.username
-    var password = req.body.password
-
-    if (username | password == undefined) {
-        return res.status(400).send({ message: 'Bad Request' })
-    }
-
-    if (password != '1234') {
-        return res.status(403).send({ message: 'Forbidden' })
-    }
-
-    try {
-        var token = await jwt.sign({ username })
-        res.cookie('jwt', token)
-        res.status(200).send({ message: 'Authenticated!' })
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send({ message: 'Internal server error' })
-    }
-}
-
-module.exports.extendToken = async (req, res) => {
-    var token = req.cookies.jwt
-    if (!token) return res.status(403).send({ message: 'Forbidden' })
-
-    try {
-        var data = await jwt.verify(token)
-        var newToken = await jwt.sign({ username: data.username })
-
-        res.cookie('jwt', newToken)
-        res.status(200).send({ message: 'Extended!' })
-    } catch (error) {
-        console.log(error.message)
-
-        if (error.message != 'jwt expired') {
-            return res.status(500).send({ message: 'Internal server error' })
-        }
-
-        res.status(403).send({ message: 'Forbidden' })
-    }
-}
 
 module.exports.getProducts = async (req, res) => {
     try {
