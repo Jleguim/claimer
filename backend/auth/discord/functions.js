9dae750a5f0bf8da911af0a46790405a86bd4f4f
Javier Leguizamo <jlegui745@gmail.com>
2022-07-07 19:55:49 -0500

    Create Discord auth functionality

---
+++ backend/auth/discord/functions.js
@@ -0,0 +1,33 @@
+const requests = require('superagent')
+
+module.exports.clientId = process.env.CLIENT_ID
+module.exports.clientSecret = process.env.CLIENT_SECRET
+module.exports.scopes = ['identify']
+// should probably change this to a config file or something
+module.exports.redirectUrl = encodeURI(`http://localhost:1337${process.env.DISCORD_CALLBACK_ROUTE}`)
+module.exports.authUrl = `https://discord.com/oauth2/authorize?response_type=code&prompt=consent&client_id=${this.clientId}&scope=${this.scopes}&redirect_uri=${this.redirectUrl}`
+
+module.exports.getUser = (access_token) => {
+    return new Promise((resolve, reject) => {
+        requests.get('https://discord.com/api/v9/users/@me')
+            .set('Authorization', 'Bearer ' + access_token)
+            .then(res => resolve(res.body))
+            .catch(reject)
+    })
+}
+
+module.exports.requestToken = (code) => {
+    return new Promise((resolve, reject) => {
+        requests.post('https://discord.com/api/oauth2/token')
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .send({
+                client_id: this.clientId,
+                client_secret: this.clientSecret,
+                redirect_uri: this.redirectUrl,
+                grant_type: 'authorization_code',
+                code: code
+            })
+            .then(res => resolve(res.body))
+            .catch(reject)
+    })
+}
---
+++ backend/auth/discord/index.js
@@ -0,0 +1,7 @@
+const functions = require('./functions.js')
+
+module.exports = {
+    authUrl: functions.authUrl,
+    requestToken: functions.requestToken,
+    getUser: functions.getUser
+}
